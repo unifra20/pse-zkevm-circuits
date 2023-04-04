b1779749428d0f143254fe5d60c28799ea2bc081
Leo Lara <leo@leolara.me>
2023-04-04 17:06:22 +0700

    Move chiquito circuit to right file

--- zkevm-circuits/src/bytecode_circuit.rs
+++ zkevm-circuits/src/bytecode_circuit.rs
@@ -5,9 +5,6 @@ pub mod bytecode_unroller;
 /// Bytecode circuit
 pub mod circuit;
 
-/// chiquito version of circuit
-pub mod ccircuit;
-
 /// Bytecode circuit tester
 #[cfg(any(feature = "test", test))]
 pub mod dev;
--- zkevm-circuits/src/bytecode_circuit/dev.rs
+++ zkevm-circuits/src/bytecode_circuit/dev.rs
@@ -1,6 +1,6 @@
 use super::{
     bytecode_unroller::{unroll, UnrolledBytecode},
-    ccircuit::BytecodeCircuit,
+    circuit::BytecodeCircuit,
 };
 
 use eth_types::Field;
